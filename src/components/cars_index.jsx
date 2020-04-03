5f109e65b91454adb35244037480f3663d11f59e
Nour Souid <noursouidd@gmail.com>
2020-04-03 00:25:34 -0400

    added actoin fetch updated the reducer and called the action in the carsIndex

--- src/components/cars_index.jsx
+++ src/components/cars_index.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
 // import { Link } from 'react-router-dom';
+import { fetchCars } from '../actions/index';
 
 class CarsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchCars(this.props.garageName);
+  }
+
   renderPosts() {
     return this.props.cars.map((car) => {
       return (
-        <div className="card-product">
-          <img src="" alt="" />
-          <div className="card-product-infos">
-            <h2>{car.brand} - {car.model}</h2>
-            <p>Owner: {car.owner}</p>
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">{car.brand} - {car.model}</h2>
+            <p className="card-text">Owner: {car.owner}</p>
           </div>
         </div>
       );
@@ -28,6 +34,14 @@ class CarsIndex extends Component {
 }
 
 function mapStateToProps(state) {
-  return { cars: state.cars };
+  return {
+    cars: state.cars,
+    garageName: state.garageName
+  };
 }
-export default connect(mapStateToProps)(CarsIndex);
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchCars }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CarsIndex);
