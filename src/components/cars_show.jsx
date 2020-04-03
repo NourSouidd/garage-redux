4db1df1b988f2fff8997b3acae3cfa53b34e22d1
Nour Souid <noursouidd@gmail.com>
2020-04-03 18:32:55 -0400

    added the element to render in the show page

--- src/components/cars_show.jsx
+++ src/components/cars_show.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CarsShow extends Component {
   render() {
+    if (!this.props.car) {
+      return <p>Loading...</p>;
+    }
     return (
-      <h1>hey</h1>
+      <div>
+        <div className="post-item">
+          <h2>{this.props.car.brand} - {this.props.car.model}</h2>
+          <p> <strong>Owner:</strong> {this.props.car.owner}</p>
+          <p>{this.props.car.plate}</p>
+        </div>
+        <Link to="/">
+          Back
+        </Link>
+      </div>
     );
   }
-
 }
 
 export default CarsShow;
